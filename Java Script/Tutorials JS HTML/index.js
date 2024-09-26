e772da221d78808ab5c91a24ea2075e59c542a4c
iuligame <iuli.game@yahoo.com>
2024-09-26 14:49:07 +0300

    update

--- Java Script/Tutorials JS HTML/index.js
+++ Java Script/Tutorials JS HTML/index.js
@@ -1,1 +1,4 @@
-alert('Scripul a rulat');
+alert("scriptul a fost executat");
+console.log("codul a fost executat");
+console.warn("codul a fost executat");
+console.error("codul a foste executat");
